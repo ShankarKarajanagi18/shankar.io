36e16650b33a730614b01f6924c8a4811fdc4827
ShankarKarajanagi18 <shankar.karajanagi7397@gmail.com>
2025-10-23 23:32:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<shankar.io>/', // 👈 Replace with your actual repo name
+  base: '/shankar.io/', // 👈 Replace with your actual repo name
 })
