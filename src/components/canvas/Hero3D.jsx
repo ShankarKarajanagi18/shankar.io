52df92e821e0cf6f6033de59d0be36a7a949aa4f
ShankarKarajanagi18 <shankar.karajanagi7397@gmail.com>
2025-10-23 23:46:21 +0530

    Updates

--- src/components/canvas/Hero3D.jsx
+++
@@ -1,5 +0,0 @@
-const Hero3DCanvas = () => {
-  return null; // This removes the 3D canvas completely
-};
-
-export default Hero3DCanvas;
--- src/components/canvas/index.js
+++
@@ -1,5 +0,0 @@
-import Hero3DCanvas from "./Hero3D";
-import BallCanvas from "./Ball";
-import StarsCanvas from "./Stars";
-
-export { Hero3DCanvas, BallCanvas, StarsCanvas };
